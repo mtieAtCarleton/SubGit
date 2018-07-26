874c740a99cafd946d557d023a93737529315dcd
bergerg <bergerg@cmc305-2016.mathcs.carleton.edu>
2018-07-26 11:08:35 -0500

    initial commit, this time with files

---
+++ upload/views.py
@@ -0,0 +1,18 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from upload.forms import DocumentForm
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        # if form.is_valid():
+        #     form.save()
+        #     return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'model_form_upload.html', {
+        'form': form
+    })
