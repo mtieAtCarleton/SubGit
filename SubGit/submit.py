d9cffb9023ff40b3e67a3223be343e4b3fbc7b2a
GalenWQ <galen.wq@gmail.com>
2018-07-27 10:31:29 -0500

    Test GitPython commit/push

---
+++ SubGit/submit.py
@@ -0,0 +1,12 @@
+from git import Repo
+
+repo_dir = '/Accounts/bergerg/Desktop/SubGit'
+repo = Repo(repo_dir)
+file_list = [
+    'SubGit/submit.py'
+]
+commit_message = 'Test GitPython commit/push'
+repo.index.add(file_list)
+repo.index.commit(commit_message)
+origin = repo.remote('origin')
+origin.push()
