65ae185928b67014a79cbcb11cc4ddfda18da27f
kemptond <kemptond@cmc305-2016.mathcs.carleton.edu>
2019-06-25 16:22:50 -0500

    added another class for testing purposes

--- setup_courses.py
+++ setup_courses.py
@@ -10,9 +10,16 @@ import pytz
 test_prof = Person(username="dln", full_name="David Liben-Nowell")
 test_prof.save()
 
+test_prof2 = Person(username="ealexander", full_name="Eric Alexander")
+test_prof2.save()
+
 intro, new = Course.objects.get_or_create(id="cs111.00-f18", number="CS 111", section="00",
                              title="Introduction to Computer Science",
                              prof=test_prof)
+
+intro2, new = Course.objects.get_or_create(id="cs201.00-f18", number="CS 201", section="00",
+                             title="Data Structures",
+                             prof=test_prof2)
 
 #Assignment.objects.get_or_create(title="Arrayed Against Us", course=ds1,
                                  #deadline=datetime(2019, 5, 27, 22, tzinfo=pytz.UTC))
